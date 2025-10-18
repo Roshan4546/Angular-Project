ddbfa5c107aa086fc77f8c48aa49886a66310b99
Roshan4546 <roshanpatra275@gmail.com>
2025-10-18 11:28:26 +0530

    blog and doctors page bug fixed

--- src/app/docters/docters.spec.ts
+++ src/app/docters/docters.spec.ts
@@ -1,7 +1,23 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { Docters } from './docters';
+import { Docters } from './docters';
 
-// describe('Docters', () => {
-    
-// });
+describe('Docters', () => {
+    let component: Docters;
+    let fixture: ComponentFixture<Docters>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [Docters]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(Docters);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
